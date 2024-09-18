6a52cb39e62362f9d10e91fb048e59b90aae8b62
johannaostling <johanna.ostling@gritacademy.se>
2024-09-18 14:17:31 +0200

    fixed version

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-let home= document.getElementsByTagName("a")[0]
-let about= document.getElementsByTagName("a")[1]
-let contact= document.getElementsByTagName("a")[2]
+let home= document.getElementsByTagName("a")[1]
+let about= document.getElementsByTagName("a")[2]
+let contact= document.getElementsByTagName("a")[3]
 
-window.onload= fetchPage("home.html")
-home.addEventListener('click', event => {
+let homes= document.getElementsByTagName("a")[4]
+let abouts= document.getElementsByTagName("a")[5]
+let contacts= document.getElementsByTagName("a")[6]
+
+// Funktion for home
+home.addEventListener('click', handlehome)
+homes.addEventListener('click', handlehome)
+function handlehome(event) {
     event.preventDefault()
     fetchPage("home.html");
-    history.pushState({ page: 0 }, "home", "/home.html");
-})
+    history.pushState({ page: 1 }, "home", "/home.html");
+}
 
-about.addEventListener('click', event => {
+// Funktion for about
+about.addEventListener('click', handleabout)
+abouts.addEventListener('click', handleabout)
+
+function handleabout(event) {
     event.preventDefault()
     fetchPage("about.html");
-    history.pushState({ page: 1 }, "about", "/about.html");
-})
+    history.pushState({ page: 2 }, "about", "/about.html");
+}
 
-contact.addEventListener('click', event=>{
+// function for contact
+contact.addEventListener('click', handlecontact)
+contacts.addEventListener('click', handlecontact)
+
+function handlecontact(event){
     event.preventDefault();
     fetchPage("contact.html")
-    history.pushState({ page: 2 }, "contact", "/contact.html");
-})
+    history.pushState({ page: 3 }, "contact", "/contact.html");
+}
 
 let content=document.getElementById("container")
 
